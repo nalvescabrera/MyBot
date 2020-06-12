708782b50a8c2fa8b77359132c8c57a38dfa81b7
Natalia <n.alvescabrera@gmail.com>
2020-06-11 23:17:08 -0300

    Primeiras etapas

---
+++ Bot.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class Bot{
+        public Bot(){
+            JFrame frame = new JFrame();
+        }
+    public static void main(String[] args) {
+        new Bot();
+        
+        
+    }
+    
+}
