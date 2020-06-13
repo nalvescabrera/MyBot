247140ce6e69c97ff9e46fc96be5381fa9e2ddeb
Natalia <n.alvescabrera@gmail.com>
2020-06-12 21:26:16 -0300

    ChatBot em Java

--- src/Bot/Bot.java
+++ src/Bot/Bot.java
@@ -11,34 +11,64 @@ public class Bot extends JFrame {
     private JTextField chatBox = new JTextField();
     
     
-    public Bot(){
+    public Bot() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setVisible(true); 
+        frame.setVisible(true);
         frame.setResizable(false);
         frame.setLayout(null);
-        frame.setSize(600,600);
+        frame.setSize(600, 600);
         frame.setTitle("ChatBot");
         frame.add(chatArea);
         frame.add(chatBox);
 
         //Para TextArea
         chatArea.setSize(500, 400);
-        chatArea.setLocation(2,2);
+        chatArea.setLocation(2, 2);
 
         //Para TextField
-        chatBox.setSize(540,30);
-        chatBox.setLocation(2,500);
+        chatBox.setSize(540, 30);
+        chatBox.setLocation(2, 500);
 
         chatBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                String gtext = chatBox.getText().toLowerCase();
+                chatArea.append("Você -->" + gtext + "\n");
+                chatBox.setText("");
 
+                if (gtext.contains("oi")) {
+                    bot("Ola");
+                } else {
+                    int rand = (int) (Math.random() + 3 + 1);
+                    if (rand == 1) {
+                        bot("Eu não entendo você");
+                    } else if (rand == 2) {
+                        bot("Eu não entendo você");
+                    } else if (rand == 1) {
+                        bot("Por favor tente de novo");
+                    }
+                    }
 
-            }
+                if (gtext.contains("como")) {
+                    bot("Ola");
+                } else {
+                    int rand = (int) (Math.random() + 3 + 1);
+                    if (rand == 1) {
+                        bot("Eu não entendo você");
+                    } else if (rand == 2) {
+                        bot("Eu não entendo você");
+                    } else if (rand == 1) {
+                        bot("Por favor tente de novo");
+                    }
+                }
+                }
         });
-
     }
+    private void bot(String string){
+        chatArea.append("BOT -->" +string+ "\n");
+        }
+
     
     public static void main(String args[]){
         new Bot();
